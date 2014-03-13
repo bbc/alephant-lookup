e0ebcb50e81999c33d58c62844ee082ea56e7c1f
Robert Kenny <kenoir@gmail.com>
2014-03-13 11:14:38 +0000

    testing read

--- spec/lookup_spec.rb
+++ spec/lookup_spec.rb
@@ -23,13 +23,62 @@ describe Alephant::Lookup do
     end
 
     describe '#read(id, opts, batch_version)' do
-      it 'does not fail' do
-        pending
+      let(:expected_query) do
+        {
+          :table_name=>"table_name",
+          :consistent_read=>true,
+          :select=>"SPECIFIC_ATTRIBUTES",
+          :attributes_to_get=>["location"],
+          :key_conditions=>{
+            "component_key"=> {
+              :comparison_operator=>"EQ",
+              :attribute_value_list=>[{"s"=>"id/9dd916afd5516828a91d259967fd394a"}]
+            },
+            "batch_version"=>{
+              :comparison_operator=>"EQ",
+              :attribute_value_list=>[{"n"=>"{:variant=>\"foo\"}"}]
+            }
+          }
+        }
+      end
+
+      it 'queries DynamoDb and returns a location' do
+        AWS::DynamoDB::Client::V20120810
+          .any_instance
+          .stub(:initialize)
+
+        AWS::DynamoDB::Client::V20120810
+          .any_instance
+          .should_receive(:query)
+          .with(expected_query)
+          .and_return(
+            {
+              :count => 1,
+              :member => [
+                { 'location' => { :s => '/location' } }
+              ]
+            }
+          )
+
+        table = double().as_null_object
+        table.stub(:table_name).and_return('table_name')
+
+        instance = subject.new(table)
+        lookup = instance.read('id',{:variant => "foo"},0)
+
+        expect(lookup.location).to eq('/location')
       end
     end
 
     describe '#write(opts, location)' do
       it 'does not fail' do
+        AWS::DynamoDB
+          .any_instance
+          .stub(:initialize)
+          .and_return(
+            double().as_null_object
+          )
+
         pending
       end
     end
