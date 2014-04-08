a63f1218f70579f362aaeb3a6a420d57253d195f
Robert Kenny <kenoir@gmail.com>
2014-04-08 19:35:37 +0100

    better logging

--- lib/alephant/lookup.rb
+++ lib/alephant/lookup.rb
@@ -2,13 +2,16 @@ require "alephant/lookup/version"
 require "alephant/lookup/lookup_helper"
 require "alephant/lookup/lookup_table"
 
+require 'alephant/logger'
+
 module Alephant
   module Lookup
+    include ::Alephant::Logger
     @@lookup_tables = {}
 
-    def self.create(table_name)
+    def self.create(table_name, logger = nil)
       @@lookup_tables[table_name] ||= LookupTable.new(table_name)
-      LookupHelper.new(@@lookup_tables[table_name])
+      LookupHelper.new(@@lookup_tables[table_name], logger)
     end
   end
 end
--- lib/alephant/lookup/lookup_helper.rb
+++ lib/alephant/lookup/lookup_helper.rb
@@ -1,21 +1,33 @@
 require "alephant/lookup/lookup_table"
 require "alephant/lookup/lookup_query"
 
+require 'alephant/logger'
+
 module Alephant
   module Lookup
     class LookupHelper
+      include Logger
+
       attr_reader :lookup_table
 
-      def initialize(lookup_table)
+      def initialize(lookup_table, logger)
+        Logger.set_logger(logger) unless logger.nil?
+
+        logger.info "LookupHelper#initialize(#{lookup_table.table_name})"
+
         @lookup_table = lookup_table
         @lookup_table.create
       end
 
       def read(id, opts, batch_version)
+        logger.info "LookupHelper#read(#{id}, #{opts}, #{batch_version})"
+
         LookupQuery.new(lookup_table.table_name, id, opts, batch_version).run!
       end
 
       def write(id, opts, batch_version, location)
+        logger.info "LookupHelper#write(#{id}, #{opts}, #{batch_version}, #{location})"
+
         LookupLocation.new(id, opts, batch_version, location).tap do |l|
           lookup_table.table.batch_put([
             {
