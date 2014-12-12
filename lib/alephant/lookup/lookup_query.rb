083edf2a6e407a8a83c539ae05a50a9c7c1fe52d
Integralist <mark.mcdx@gmail.com>
2014-12-12 11:31:52 +0000

    Additional Logging

--- lib/alephant/lookup/lookup_query.rb
+++ lib/alephant/lookup/lookup_query.rb
@@ -1,22 +1,25 @@
 require "crimp"
 require "alephant/lookup/lookup_location"
+require "alephant/logger"
 
 module Alephant
   module Lookup
     class LookupQuery
+      include Logger
       attr_reader :table_name, :lookup_location
 
       def initialize(table_name, component_id, opts, batch_version)
         @client = AWS::DynamoDB::Client::V20120810.new
         @table_name = table_name
         @lookup_location = LookupLocation.new(component_id, opts, batch_version)
+        logger.info "LookupQuery#initialize: table name '#{table_name}', component id '#{component_id}', batch version '#{batch_version}', location '#{lookup_location}'"
       end
 
       def run!
         lookup_location.tap do |l|
           l.location = s3_location_from(
             @client.query(to_q)
-          )
+          ).tap { |loc| logger.info "LookupQuery#run!: location '#{loc}'" }
         end
       end
 
--- lib/alephant/lookup/lookup_table.rb
+++ lib/alephant/lookup/lookup_table.rb
@@ -3,19 +3,24 @@ require "thread"
 require "timeout"
 
 require "alephant/support/dynamodb/table"
+require "alephant/logger"
 
 module Alephant
   module Lookup
     class LookupTable < ::Alephant::Support::DynamoDB::Table
+      include Logger
       attr_reader :table_name, :client
 
       def initialize(table_name)
         @mutex      = Mutex.new
         @client     = AWS::DynamoDB::Client::V20120810.new
         @table_name = table_name
+        logger.info "LookupTable#initialize: table name '#{table_name}'"
       end
 
       def write(component_key, version, location)
+        logger.info "LookupTable#write: component key '#{component_key}', version '#{version}', location '#{location}'"
+
         client.put_item({
           :table_name => table_name,
           :item => {
